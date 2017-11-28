216ba845ff42d8029227cba00f45d915a76ad483
SungMin Park <rnfxl92@naver.com>
2017-11-28 14:07:29 +0900

    aaa
    aaa

--- lecture-master/build/generated-sources/jaxb/aeat/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.5-2 버전을 통해 생성되었습니다. 
-// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
-// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
-// 생성 날짜: 2017.11.21 시간 02:06:48 PM KST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "tag:atsc.org,2016:XMLSchemas/ATSC3/Delivery/AEAT/1.0/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package aeat;
